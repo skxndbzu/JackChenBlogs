7ed7a8163e1e9a5023a87bdec6f6c69074d9e0bf
陈铭鹤 <615880048@qq.com>
2024-10-29 16:31:36 +0800

    持续更新面试模块

--- src/.vuepress/siderbar/interviewExperience.ts
+++ src/.vuepress/siderbar/interviewExperience.ts
@@ -2,23 +2,23 @@ import { arraySidebar } from "vuepress-theme-hope";
 
 export const interviewExperience = arraySidebar([
     {
-        text: "北京鑫万家科技笔试",
-        link: "disable",
+        text: "北京鑫万家科技",
+        link: "北京鑫万家科技",
         icon: "book",
     },
     {
         text: "北京中科软",
-        link: "encrypt.md",
+        link: "北京中科软",
         icon: "book",
     },
     {
         text: "安硕信息",
-        link: "layout.md",
+        link: "安硕信息",
         icon: "book",
     },
     {
         text: "甘肃中工国际",
-        link: "markdown.md",
+        link: "甘肃中工国际",
         icon: "book",
     },
     {
